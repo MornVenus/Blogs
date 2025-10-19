52c0389f2ab27c8328f87436509daf80c459221a
venus <995047027@qq.com>
2025-10-20 06:32:38 +0800

    add project: GraphicSample.csproj

---
+++ dotnet/GraphicSample/GraphicSample/Views/RectMemView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace GraphicSample.Views;
+
+public partial class RectMemView : UserControl
+{
+    public RectMemView()
+    {
+        InitializeComponent();
+        this.DataContext = this;
+    }
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(RectMemView), new PropertyMetadata(""));
+
+    public Brush BackBrush
+    {
+        get { return (Brush)GetValue(BackBrushProperty); }
+        set { SetValue(BackBrushProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for BackBrush.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty BackBrushProperty =
+        DependencyProperty.Register("BackBrush", typeof(Brush), typeof(RectMemView), new PropertyMetadata(default));
+}
---
+++ dotnet/MemorySamples/MemoryObjectModelSample/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+
+namespace MemoryObjectModelSample;
+
+public class Student
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public Student(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void StringBtn_Click(object sender, RoutedEventArgs e)
+    {
+        string s = "";
+        string str0 = "9";
+        string str = "venus是我1234";
+        string str2 = "56789";
+        MessageBox.Show(str);
+    }
+
+    private void ClassBtn_Click(object sender, RoutedEventArgs e)
+    {
+        Student stu = new Student(1, "venus是我1234");
+    }
+
+    private void ArrayBtn_Click(object sender, RoutedEventArgs e)
+    {
+        int[] intArray = [1, 2, 3, 4, 5];
+        string[] strArray = ["111", "222", "333", "444", "555"];
+        Student[] stuArray = [new Student(1, "venus111"), new Student(2, "venus222"), new Student(3, "venus333"), new Student(4, "venus444")];
+    }
+
+    private void ListBtn_Click(object sender, RoutedEventArgs e)
+    {
+        List<int> intList = [1, 2, 3, 4, 5];
+        List<string> strList = ["111", "222", "333", "444", "555"];
+        List<Student> stuList = [new Student(1, "venus111"), new Student(2, "venus222"), new Student(3, "venus333"), new Student(4, "venus444")];
+    }
+
+    private void DictionaryBtn_Click(object sender, RoutedEventArgs e)
+    {
+        Dictionary<string, Student> dic = new Dictionary<string, Student>();
+        dic.Add("111", new Student(1, "venus111"));
+        dic.Add("222", new Student(2, "venus222"));
+        dic.Add("333", new Student(3, "venus333"));
+    }
+}
